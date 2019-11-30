348772dbcc3216c3aa69690b4725538a7280b233
Mohtasim Chowdhury <mohtasim317@hotmail.com>
2019-11-30 15:33:08 -0500

    Frontend up and running

--- app/index.jsx
+++ app/index.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-// class App extends React.Component{
-//     contructor(props) {
-//         super(props);
-//         this.state = {
-//             item = '',
-//             list = []
-//         }
-//     }
-//     render() {
-//         return (
-//             <div>
-//                 <h1>To-Do List</h1>
-//             </div>
-//         )
-//     }
-// }
+class App extends React.Component{
+    // contructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         item = '',
+    //         list = []
+    //     }
+    // }
+    // render() {
+    //     return (
+    //         <div>
+    //             <h1>To-Do List</h1>
+    //         </div>
+    //     )
+    // }
+}
+
+ReactDom.render(<App />, document.getElementById('app'))
