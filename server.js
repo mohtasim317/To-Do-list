df7d86fe21b7341944d618761e63ae461362e2a9
Mohtasim Chowdhury <mohtasim317@hotmail.com>
2020-05-09 22:59:53 -0400

    added stuff

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = 1738;
+const db = require("./db");
+const collection = "todo";
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use("/static", express.static(path.join(__dirname, "./static")));
 
 app.get("/", (req, res) => {
