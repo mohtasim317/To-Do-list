07a7fb4438c993898161661de5fa59d06d56cce5
Mohtasim Chowdhury <mohtasim317@hotmail.com>
2020-05-21 17:07:04 -0400

    Changed error handling

--- server.js
+++ server.js
@@ -3,18 +3,12 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = 1738;
-const db = require("./db");
-const collection = "todo";
 
 app.use(bodyParser.json());
 app.use("/static", express.static(path.join(__dirname, "./static")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.get("/*", (req, res) => {
-  throw new Error("Oops something broke :/");
 });
 
 app.listen(PORT);
