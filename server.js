bc3006523057bedff7909e36c25b9552a1a17a0e
Mohtasim Chowdhury <mohtasim317@hotmail.com>
2020-06-07 20:53:22 -0400

    database connection

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const path = require("path");
-const bodyParser = require("body-parser");
 const app = express();
 const PORT = 1738;
+const mongoose = require("mongoose");
+require("dotenv/config");
 
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use("/static", express.static(path.join(__dirname, "./static")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get("/posts", (req, res) => {
+  res.send("We're on posts");
+});
+
+mongoose.connect(process.env.DB_connection, { useNewUrlParser: true }, () => {
+  console.log("we connected");
+});
+
 app.listen(PORT);
+
+//
