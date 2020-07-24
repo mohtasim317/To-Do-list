848876939bcb517dbb86d0bbd3785bb66377feb2
Mohtasim Chowdhury <mohtasim317@hotmail.com>
2020-07-24 17:39:10 -0400

    CRD, just need U

--- static/todo.js
+++ static/todo.js
@@ -3,34 +3,36 @@ const list = document.querySelector(".main-list");
 const button = document.querySelector(".button");
 let text = "";
 
-fetch("http://localhost:1738/posts/")
-  .then((res) => res.json())
-  .then((res) =>
-    res.map((object) => {
-      const listItem = document.createElement("li");
-      const removeButton = document.createElement("button");
-      if (object.item !== "") {
-        listItem.innerText = object.item;
-        listItem.className = "list-item";
-        removeButton.className = "remove-button";
-        removeButton.innerText = "x";
-        removeButton.id = object._id;
-        removeButton.addEventListener("click", (event) => {
-          fetch(`http://localhost:1738/posts/${removeButton.id}`, {
-            method: "DELETE",
-            body: JSON.stringify({ _id: removeButton.id }), //jsn stringify
-            headers: {
-              "Content-Type": "application/json",
-            },
-            credentials: "same-origin",
-          }).then((res) => console.log(res));
-          listItem.remove();
-        });
-        listItem.appendChild(removeButton);
-        list.appendChild(listItem);
-      }
-    })
-  );
+(function getdata() {
+  fetch("http://localhost:1738/posts/")
+    .then((res) => res.json())
+    .then((res) =>
+      res.map((object) => {
+        const listItem = document.createElement("li");
+        const removeButton = document.createElement("button");
+        if (object.item !== "") {
+          listItem.innerText = object.item;
+          listItem.className = "list-item";
+          removeButton.className = "remove-button";
+          removeButton.innerText = "x";
+          removeButton.id = object._id;
+          removeButton.addEventListener("click", (event) => {
+            fetch(`http://localhost:1738/posts/${removeButton.id}`, {
+              method: "DELETE",
+              body: JSON.stringify({ _id: removeButton.id }), //jsn stringify
+              headers: {
+                "Content-Type": "application/json",
+              },
+              credentials: "same-origin",
+            }).then((res) => console.log(res));
+            listItem.remove();
+          });
+          listItem.appendChild(removeButton);
+          list.appendChild(listItem);
+        }
+      })
+    );
+})();
 
 textField.addEventListener("input", (event) => {
   if (textField.value !== null) text = textField.value;
@@ -63,3 +65,5 @@ function adding(event) {
 }
 
 button.addEventListener("click", adding);
+
+//
