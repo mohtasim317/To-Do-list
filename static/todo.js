68e6b25816419e5e148ecd04524338211d3796d2
Mohtasim Chowdhury <mohtasim317@hotmail.com>
2020-07-24 16:01:40 -0400

    some frontend backend connection

--- static/todo.js
+++ static/todo.js
@@ -3,11 +3,31 @@ const list = document.querySelector(".main-list");
 const button = document.querySelector(".button");
 let text = "";
 
+fetch("http://localhost:1738/posts/")
+  .then((res) => res.json())
+  .then((res) =>
+    res.map((object) => {
+      const listItem = document.createElement("li");
+      const removeButton = document.createElement("button");
+      if (object.item !== "") {
+        listItem.innerText = object.item;
+        listItem.className = "list-item";
+        removeButton.className = "remove-button";
+        removeButton.innerText = "x";
+        removeButton.addEventListener("click", (event) => {
+          listItem.remove();
+        });
+        listItem.appendChild(removeButton);
+        list.appendChild(listItem);
+      }
+    })
+  );
+
 textField.addEventListener("input", (event) => {
   if (textField.value !== null) text = textField.value;
 });
 
-button.addEventListener("click", (event) => {
+function adding(event) {
   const listItem = document.createElement("li");
   const removeButton = document.createElement("button");
   if (text !== "") {
@@ -20,7 +40,15 @@ button.addEventListener("click", (event) => {
     });
     listItem.appendChild(removeButton);
     list.appendChild(listItem);
+    fetch("http://localhost:1738/posts/", {
+      method: "POST",
+      body: { item: text },
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res));
     text = "";
     textField.value = "";
   }
-});
+}
+
+button.addEventListener("click", adding);
